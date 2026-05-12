cad21fa292c2e13d657bbd692170dd9a743126aa
DanielAngel2003 <daniel.angel_2003@outlook.com>
2026-05-12 09:29:32 -0600

    Update Rossler_atractor_simulator.py
    Ajuste de parámetros para una mejor dvisualización del contenido, aasí como ajuste de variables con base a los experimentos del libro 'A first course in Systems in Biology' ejercicio 4.66

--- Rossler_atractor_simulator.py
+++ Rossler_atractor_simulator.py
@@ -20,7 +20,7 @@ import turtle
 # Visualizador, Pantalla
 window = turtle.Screen()
 turtle.setup(675, 1200)
-turtle.setworldcoordinates(-20, -20, 20, 20)
+turtle.setworldcoordinates(-5, -5, 5, 5)
 turtle.bgcolor('black')
 
 # Creación del lapicero
@@ -34,16 +34,16 @@ turtle.tracer(10, 0)
 # Definir parámetros. La práctica 4.66 indica:
 ### a=0.36, b=0.4, c=4.5
 
-a, b, c = 0.1, 0.1, 18.0
-#a, b, c = 0.36, 0.4, 4.5
+#a, b, c = 0.1, 0.1, 18.0
+a, b, c = 0.36, 0.4, 4.5
 
 # Cambio en la posición del lapiz
 dt = 0.01
 
 # Condiciones iniciales. La práctica 4.66 indica:
 
-x, y, z = 0.1, 0.1, 0.1
-#x, y, z = 0, -3, 0
+#x, y, z = 0.1, 0.1, 0.1
+x, y, z = 0, -3, 3
 
 ### main loop for pencil
 if __name__ == '__main__':
